fe3c3b4f92b23dce9aaf5e41e11fe4370423e289
Pr454th <prasathkarthiban1999@gmail.com>
2023-03-27 12:24:45 +0530

    github scrapping

--- news_scrapper/news_scrapper/spiders/github.py
+++ news_scrapper/news_scrapper/spiders/github.py
@@ -2,17 +2,31 @@ import scrapy
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
 from news_scrapper.items import companyItem
-
 class GithubSpider(scrapy.Spider):
     name = "github"
     allowed_domains = ["github.com"]
-    start_urls = ["https://github.com/hxu296/leetcode-company-wise-problems-2022"]
-    custom_settings={
-        "FEED_URI":"companies.json",
-        "FEED_FORMAT":"json",
+    start_urls = [
+        "https://github.com/hxu296/leetcode-company-wise-problems-2022"]
+    custom_settings = {
+        "FEED_URI": "companies.json",
+        "FEED_FORMAT": "json",
     }
+
     def parse(self, response):
-        company=companyItem()
-        company["name"]=response.xpath("//a[@role='menuitem']/text()").getall()
-        company["questions"]=response.xpath("//a[@rel='nofollow']/@href").getall()
+        company = companyItem()
+        companies= response.xpath(
+            "//a[@data-targets='readme-toc.entries']/text()").getall()
+        index=2
+        for table in response.xpath("//table"):
+            company['name']=companies[index]
+            index+=1
+            company['questions'] = []
+            for row in table.xpath(".//tbody//tr"):
+                questionSet=[]
+                for col in row.xpath(".//td//a[@rel='nofollow']"):
+                    # print(col.xpath(".//@href").get())
+                    questionSet.append(col.xpath(".//@href").get())
+                    questionSet.append(col.xpath(".//text()").get())
+                company['questions'].append(questionSet)
+            yield company
         return company
