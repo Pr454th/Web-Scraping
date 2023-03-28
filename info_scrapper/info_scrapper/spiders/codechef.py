1c21263a530ae51f2f60ced05879d092325a13a0
pr454th <prasathkarthiban1999@gmail.com>
2023-03-28 21:19:21 +0530

    codechef scrapping

--- info_scrapper/info_scrapper/spiders/codechef.py
+++ info_scrapper/info_scrapper/spiders/codechef.py
@@ -6,17 +6,19 @@ import json
 class CodechefSpider(scrapy.Spider):
     name = "codechef"
     allowed_domains = ["codechef.com"]
-    start_urls = ["http://codechef.com/"]
+    start_urls = ["https://www.codechef.com/api/list/problems?page=7&limit=50&sort_by=difficulty_rating&sort_order=asc&search=&category=rated&start_rating=0&end_rating=999&topic=&tags=&group=all&="]
     custom_settings={
-        "FEED_URI":"codechef.csv",
-        "FEED_FORMAT":"csv",
+        "FEED_URI":"codechef.json",
+        "FEED_FORMAT":"json",
     }
-    def parse(self, response):
-        print("hello world")
-        url="https://www.codechef.com/api/list/problems?page=7&limit=50&sort_by=difficulty_rating&sort_order=asc&search=&category=rated&start_rating=0&end_rating=999&topic=&tags=&group=all&="
+    def start_requests(self):
+        # Define the cookies to send with the request
         cookies = {
             "Cookie":"SESS93b6022d778ee317bf48f7dbffe03173=759139e356ef1a1497969cdde6562fb3"
         }
-        res=requests.get(url,cookies=cookies)
-        data=json.loads(res.content)
+        # Make the request with the cookies
+        for url in self.start_urls:
+            yield scrapy.Request(url, cookies=cookies, callback=self.parse)
+    def parse(self, response):
+        data=json.loads(response.text)
         return data['data']
